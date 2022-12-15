6986a223b2e2a1e8d3101bf027fa7936c2d60324
Leebyeongyong <95388513+Leebyeongyong@users.noreply.github.com>
2022-12-15 17:03:12 +0900

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir : "../src/main/resources/static",   //빌드 타겟디렉토리
+  devServer : {
+    proxy : {
+      '/test' : {
+        //test로 들어오면 스프링서버(8888)로 보냄
+        target : 'http://localhost:8888',
+        changeOrigin : true //cross origin 허용
+      }
+    }
+  }
 })
